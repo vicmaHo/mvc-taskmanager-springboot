621124b3a1ffd08f5cbe6592975f1ed71bec25c0
Victor Hernandez <victormanu968@gmail.com>
2023-11-23 21:15:00 -0500

    commit inicial

---
+++ src/main/java/com/hernandezvictor/ProyectoMVC/ProyectoMvcApplication.java
@@ -0,0 +1,13 @@
+package com.hernandezvictor.ProyectoMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoMvcApplication.class, args);
+	}
+
+}
