e2dc4ff1663b768e15e62ab70168c551e83ca135
Victor Hernandez <victormanu968@gmail.com>
2024-01-03 16:23:37 -0500

    Probando GET con parametros

--- src/main/java/com/hernandezvictor/ProyectoMVC/Controller/TaskController.java
+++ src/main/java/com/hernandezvictor/ProyectoMVC/Controller/TaskController.java
@@ -20,6 +20,7 @@ import com.hernandezvictor.ProyectoMVC.Model.Task;
 public class TaskController {
     
     private List<Task> tasks = new ArrayList<>(); // Array donde almaceno los datos de las tareas
+    private String mensaje = "Aun no tenemos tus datos";
     
     
     /**
@@ -31,6 +32,7 @@ public class TaskController {
     @GetMapping("/") // con el getmapping se indica que se encargue de responder las solicitudes HTTP GET dirigidas a la URL "/"
     public String index(Model model){ // model es una interfaz de Spring que se encarga de manejar los datos de la vista, 
         model.addAttribute("tasks", tasks); // se le pasan los atributos con sus valores y luego se retorna la vista que será renderizada 
+        model.addAttribute("identidad", mensaje);
         return "index";
     }
 
@@ -69,6 +71,29 @@ public class TaskController {
         tasks.add(new Task("PushUps"));
         tasks.add(new Task("PullUps"));
         tasks.add(new Task("Core"));
+        return "redirect:/";
+    }
+
+
+    /**
+     * Funcion de prueba para solicitudes Get que requieren parametros por URL
+     * @param nombre
+     * @param edad
+     * @param model
+     * @return
+     */
+    @GetMapping("/identidad")
+    public String addID(
+            @RequestParam(name = "nombre", defaultValue = "Invitado") String nombre,
+            @RequestParam(name = "edad", defaultValue = "0") int edad,
+            Model model) {
+
+        mensaje = "Hola, " + nombre + ". Tu edad es " + edad + " años.";
+
+        // // Agregar el mensaje al modelo para mostrar en la vista
+        // model.addAttribute("identidad", mensaje);
+
+        // Devolver el nombre de la vista
         return "redirect:/";
     }
 }   
